ac68722b249fe12ad1dce20d0cbfabdd68c3145d
cemfcelebi <cemfcelebi@gmail.com>
2021-04-12 00:20:49 +0300

    Configured for deployment

--- profile_project/settings.py
+++ profile_project/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +27,10 @@ SECRET_KEY = '3$3ckhc=f*r0vl57xh4ve*r$$jbsbai&y&g)33(v7#m#+v^#3n'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'ec2-3-143-116-93.us-east-2.compute.amazonaws.com',
+    '127.0.0.1'
+    ]
 
 
 # Application definition
